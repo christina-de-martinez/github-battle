667af7d921149f69c9679d89f29246b5a98513b3
Christina de Martínez <christina.martinez@faithlife.com>
2021-05-18 19:46:48 +0200

    Add hover tooltip and dark mode.

--- app/index.js
+++ app/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Popular from './components/Popular';
+import Battle from './components/Battle';
+import Nav from './components/Nav';
+import { ThemeProvider } from './contexts/theme'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
   render() {
     return (
-    <div className='container'>
-      <Battle />
-    </div>
+      <ThemeProvider value={this.state}>
+         <div className={this.state.theme}>
+           <div className='container'>
+             <Nav />
+             <Battle />
+           </div>
+          </div>
+      </ThemeProvider>
   )
   }
 }
