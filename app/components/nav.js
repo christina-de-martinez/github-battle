8423e7d9982b11e9cf6954538ee727a385550b0a
Christina de Martínez <christina.martinez@faithlife.com>
2021-06-14 20:12:11 +0200

    Added routing.

--- app/components/nav.js
+++ app/components/nav.js
@@ -1,20 +1,38 @@
 import * as React from 'react'
- import { ThemeConsumer } from '../contexts/theme'
+import { ThemeConsumer } from '../contexts/theme'
+import { NavLink } from 'react-router-dom'
 
- export default function Nav () {
-   return (
-     <ThemeConsumer>
-       {({ theme, toggleTheme }) => (
-         <nav className='row space-between'>
-           <button
-             style={{fontSize: 30}}
-             className='btn-clear'
-             onClick={toggleTheme}
-           >
-             {theme === 'light' ? '🌚' : '☀️'}
-           </button>
-         </nav>
-       )}
-     </ThemeConsumer>
-   )
- }
+const activeStyle = {
+  color: '#4F4140'
+}
+
+export default function Nav () {
+  return (
+    <ThemeConsumer>
+      {({ theme, toggleTheme }) => (
+        <nav className='row space-between'>
+          <ul className='row nav'>
+            <li>
+              <NavLink to='/' exact activeStyle={activeStyle} className='nav-link'
+              >
+                Popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/battle' activeStyle={activeStyle} className='nav-link'>
+                Battle
+              </NavLink>
+            </li>
+          </ul>
+          <button
+            style={{fontSize: 30}}
+            className='btn-clear'
+            onClick={toggleTheme}
+          >
+            {theme === 'light' ? '🌚' : '☀️'}
+          </button>
+        </nav>
+      )}
+    </ThemeConsumer>
+  )
+}
